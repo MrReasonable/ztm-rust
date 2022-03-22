047ed5fe97de3f5ac4dc510cda142dd441c24ecb
Ian Dominey <ian.dominey@gmail.com>
2022-03-22 22:46:41 +0000

    Completed activity 4.

--- activities/src/bin/a4a.rs
+++ activities/src/bin/a4a.rs
@@ -7,4 +7,10 @@
 // * Use a variable set to either true or false
 // * Use a match expression to determine which message to display
 
-fn main() {}
+fn main() {
+    let val = false;
+    match val {
+        true => println!("It's true"),
+        false => println!("It's false"),
+    }
+}
