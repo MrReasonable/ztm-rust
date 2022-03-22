33e07585ca108401c7c7ccf91d3da5eb06daf9cc
Ian Dominey <ian.dominey@gmail.com>
2022-03-22 22:19:49 +0000

    Completed activity 2

--- activities/src/bin/a2.rs
+++ activities/src/bin/a2.rs
@@ -8,4 +8,15 @@
 // * Use a function to display the result
 // * Use the "{:?}" token in the println macro to display the result
 
-fn main() {}
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+fn display_result(num: i32) {
+    println!("{:?}", num);
+}
+
+fn main() {
+    let result = add(1, 5);
+    display_result(result);
+}
