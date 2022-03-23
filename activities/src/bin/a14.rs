57d45879319ee57c799df0498a7da7bd84dd4823
Ian Dominey <ian.dominey@gmail.com>
2022-03-23 20:43:12 +0000

    Completed activity 14.

--- activities/src/bin/a14.rs
+++ activities/src/bin/a14.rs
@@ -11,4 +11,47 @@
 // * Use an if expression to determine which person's info should be printed
 // * The name and colors should be printed using a function
 
-fn main() {}
+struct Person {
+    name: String,
+    age: u8,
+    favourite_color: String,
+}
+
+fn print_it(the_string: &str) {
+    println!("{:?}", the_string);
+}
+
+fn main() {
+    let people = vec![
+        Person {
+            name: "Ian".to_owned(),
+            age: 20,
+            favourite_color: "red".to_owned(),
+        },
+        Person {
+            name: "Bob".to_owned(),
+            age: 25,
+            favourite_color: "blue".to_owned(),
+        },
+        Person {
+            name: "Jack".to_owned(),
+            age: 12,
+            favourite_color: "mauve".to_owned(),
+        },
+        Person {
+            name: "Steve".to_owned(),
+            age: 98,
+            favourite_color: "orange".to_owned(),
+        },
+    ];
+
+    for person in &people {
+        if person.age < 50 {
+            println!("Age: {}", person.age);
+            print!("Name: ");
+            print_it(&person.name);
+            print!("Favourite color: ");
+            print_it(&person.favourite_color);
+        }
+    }
+}
