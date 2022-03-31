6a69138fe39dfff7a62ed01effc27ec138d379d4
Ian Dominey <ian.dominey@gmail.com>
2022-03-31 21:34:06 +0100

    Completed activity 25.

--- activities/src/bin/a25.rs
+++ activities/src/bin/a25.rs
@@ -12,4 +12,37 @@
 // * Use a single function to print out the perimeter of the shapes
 //   * The function must utilize impl trait as a function parameter
 
-fn main() {}
+trait Perimeter {
+    fn calc_perimeter(&self) -> i32;
+}
+
+struct Square {
+    len: i32,
+}
+
+impl Perimeter for Square {
+    fn calc_perimeter(&self) -> i32 {
+        self.len * 4
+    }
+}
+
+struct Triangle {
+    a: i32,
+    b: i32,
+    c: i32,
+}
+
+impl Perimeter for Triangle {
+    fn calc_perimeter(&self) -> i32 {
+        self.a + self.b + self.c
+    }
+}
+
+fn print_perimeter(shape: impl Perimeter) {
+    println!("{}", shape.calc_perimeter());
+}
+
+fn main() {
+    print_perimeter(Square { len: 10 });
+    print_perimeter(Triangle { a: 5, b: 20, c: 53 });
+}
