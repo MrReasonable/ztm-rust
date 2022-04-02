5a1e8293bf07c0135133cf94249b88398627791f
Ian Dominey <ian.dominey@gmail.com>
2022-04-02 21:07:02 +0100

    Completed activity 28

--- activities/src/bin/a28.rs
+++ activities/src/bin/a28.rs
@@ -10,6 +10,7 @@
 // * Create a function for each type of clothes (shoes, shirt, pants)
 //   that accepts the new type specific to that type of clothing
 
+#[derive(Debug)]
 enum Color {
     Black,
     Blue,
@@ -23,4 +24,49 @@ enum Color {
     Yellow,
 }
 
-fn main() {}
+#[derive(Debug)]
+struct ShoesColor(Color);
+
+#[derive(Debug)]
+struct ShirtColor(Color);
+
+#[derive(Debug)]
+struct PantsColor(Color);
+
+impl ShoesColor {
+    pub fn new(color: Color) -> Self {
+        Self(color)
+    }
+}
+
+impl ShirtColor {
+    pub fn new(color: Color) -> Self {
+        Self(color)
+    }
+}
+impl PantsColor {
+    pub fn new(color: Color) -> Self {
+        Self(color)
+    }
+}
+
+fn print_shirt_color(color: ShirtColor) {
+    println!("Shirt color is {:?}", color);
+}
+
+fn print_shoes_color(color: ShoesColor) {
+    println!("Shoes color is {:?}", color);
+}
+fn print_pants_color(color: PantsColor) {
+    println!("Pants color is {:?}", color);
+}
+
+fn main() {
+    let shirt_color = ShirtColor::new(Color::Red);
+    let shoes_color = ShoesColor::new(Color::Black);
+    let pants_color = PantsColor::new(Color::Green);
+
+    print_shirt_color(shirt_color);
+    print_shoes_color(shoes_color);
+    print_pants_color(pants_color);
+}
