37cbe8293e5c5e28b4357e346ced4046898e2d9c
Ian Dominey <ian.dominey@gmail.com>
2022-04-02 22:03:28 +0100

    Completed activity 35.

--- activities/src/bin/a35.rs
+++ activities/src/bin/a35.rs
@@ -54,4 +54,34 @@ enum Tile {
     Wood,
 }
 
-fn main() {}
+fn print_tile(tile: Tile) {
+    use Tile::*;
+    match tile {
+        Brick(brick @ BrickStyle::Red | brick @ BrickStyle::Gray) => {
+            println!("The brick color is {:?}", brick)
+        }
+        Brick(style) => println!("{:?} brick", style),
+        Water(p) if p.0 < 10 => println!("Water pressure level: {:?}", p.0),
+        Water(_) => println!("High water pressure!"),
+        Dirt | Grass | Sand => println!("Ground tile"),
+        Treasure(TreasureChest {
+            content: TreasureItem::Gold,
+            amount: a,
+        }) if a >= 100 => println!("Lots of gold!"),
+        _ => (),
+    }
+}
+
+fn main() {
+    let tile = Tile::Water(Pressure(30));
+    print_tile(tile);
+    let tile = Tile::Brick(BrickStyle::Gray);
+    print_tile(tile);
+    let tile = Tile::Brick(BrickStyle::Dungeon);
+    print_tile(tile);
+    let tile = Tile::Treasure(TreasureChest {
+        amount: 110,
+        content: TreasureItem::Gold,
+    });
+    print_tile(tile);
+}
