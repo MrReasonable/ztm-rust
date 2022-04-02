aec2c90c97b8db012e624d8b713a2df6b69094ab
Ian Dominey <ian.dominey@gmail.com>
2022-04-02 22:26:55 +0100

    Complete exercise 36.

--- activities/src/bin/a36.rs
+++ activities/src/bin/a36.rs
@@ -14,7 +14,20 @@ fn data() -> &'static [u64] {
     &[5, 5, 4, 4, 3, 3, 1]
 }
 
+fn process_chunk(data: &[u64]) {
+    match data {
+        [first, second] => println!("{}+{}={}", first, second, first + second),
+        [single] => println!("Unpaired value: {}", single),
+        [] => println!("Data stream complete"),
+        [..] => unreachable!("Chunk size should never exceed 2"),
+    };
+}
+
 fn main() {
     // `stream` is an iterator of Option<&[u64]>
     let mut stream = data().chunks(2);
+
+    for chunk in stream {
+        process_chunk(chunk);
+    }
 }
