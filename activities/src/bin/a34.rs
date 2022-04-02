a0ebcf4d2e8f7d4e101d83609a0e04b7dd8922e7
Ian Dominey <ian.dominey@gmail.com>
2022-04-02 21:31:15 +0100

    Completed activity 34.

--- activities/src/bin/a34.rs
+++ activities/src/bin/a34.rs
@@ -16,4 +16,58 @@
 // Notes:
 // * Optionally use generics for each state
 
-fn main() {}
+#[derive(Debug, Clone, Copy)]
+struct LuggageId(usize);
+#[derive(Debug)]
+struct Luggage(LuggageId);
+#[derive(Debug)]
+struct CheckIn(LuggageId);
+#[derive(Debug)]
+struct OnLoad(LuggageId);
+#[derive(Debug)]
+struct OffLoad(LuggageId);
+#[derive(Debug)]
+struct AwaitPickup(LuggageId);
+#[derive(Debug)]
+struct EndCustody(LuggageId);
+
+impl Luggage {
+    pub fn new(id: LuggageId) -> Self {
+        Self(id)
+    }
+
+    fn check_in(self) -> CheckIn {
+        CheckIn(self.0)
+    }
+}
+
+impl CheckIn {
+    fn onload(self) -> OnLoad {
+        OnLoad(self.0)
+    }
+}
+
+impl OnLoad {
+    fn offload(self) -> OffLoad {
+        OffLoad(self.0)
+    }
+}
+
+impl OffLoad {
+    fn carousel(self) -> AwaitPickup {
+        AwaitPickup(self.0)
+    }
+}
+
+impl AwaitPickup {
+    fn pickup(self) -> (Luggage, EndCustody) {
+        (Luggage(self.0), EndCustody(self.0))
+    }
+}
+
+fn main() {
+    let id = LuggageId(10);
+    let luggage = Luggage::new(id);
+    let luggage = luggage.check_in().onload().offload().carousel().pickup();
+    println!("{:?}", luggage);
+}
