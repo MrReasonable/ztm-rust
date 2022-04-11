f3a7662af304bb140e4d2eb72e5b49c21ae32b83
Ian Dominey <ian.dominey@gmail.com>
2022-04-11 19:56:32 +0100

    Completed m3 activity.

--- activities/src/bin/demo-macro-repetitions.rs
+++ activities/src/bin/demo-macro-repetitions.rs
@@ -1,1 +1,16 @@
-fn main() {}
+macro_rules! my_vec {
+    (
+        $($element:expr),+
+        $(,)?
+    ) => {{
+        let mut v = Vec::new();
+        $(
+            v.push($element);
+        )+
+        v
+    }};
+}
+fn main() {
+    let v = my_vec![1, 2, 3, 4];
+    dbg!(v);
+}
--- activities/src/bin/m3.rs
+++ activities/src/bin/m3.rs
@@ -5,4 +5,20 @@
 //   * The macro must be able to accept multiple key/value pairs.
 //   * Print out the generated hashmap using the `dbg!` macro to ensure it works.
 
-fn main() {}
+macro_rules! hashmap {
+    (
+        $($key:expr => $val:expr),+
+        $(,)?
+    ) => {{
+        let mut hm = std::collections::HashMap::new();
+        $(
+            hm.insert($key, $val);
+        )+
+        hm
+    }};
+}
+
+fn main() {
+    let hm = hashmap! {"ian"=>32, "bob"=>13, "lucy"=>15, "michael"=>150};
+    dbg!(hm);
+}
